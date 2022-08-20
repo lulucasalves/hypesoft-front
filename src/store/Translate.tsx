8f67fc0e821afc25ffe498aa29991053a4195898
lulucasalves <lucas.alves.supus@outlook.com>
2022-08-19 23:35:36 -0300

    fixed build erros

--- src/store/Translate.tsx
+++ src/store/Translate.tsx
@@ -3,7 +3,6 @@ import { IntlProvider } from 'react-intl'
 
 import * as enMessages from '../translate/en.json'
 import * as ptMessages from '../translate/pt.json'
-import * as esMessages from '../translate/es.json'
 
 import { IMyContext, MyContext } from './Config'
 import { ILangProvider } from '../types'
